09e3734a8916d6959ce146c91b627aeec9f42552
Alla P <evetrueivory@gmail.com>
2017-08-16 19:14:52 +0300

    build a page and styles on flexbox.

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+
+const gulp            = require('gulp');
+const autoprefixer    = require('autoprefixer');
+const jslint          = require('gulp-jslint');
+const csslint         = require('gulp-csslint');
+const uglifyJS        = require('uglify-js');
+
+
+gulp.task('hello', function () {
+    console.log('Hello world')
+});
